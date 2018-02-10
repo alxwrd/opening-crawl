653ed0219dc73dd4d47ddf962cfc227f4bb6651f
Alex Ward <alxwrd@googlemail.com>
2018-02-10 18:38:35 +0000

    inital commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Opening Crawl
+
+_Based on the [dev.to](https://dev.to) post
+[Developing the Star Wars opening crawl in HTML/CSS]
+(https://dev.to/christopherkade/developing-the-star-wars-opening-crawl-in-htmlcss-2j9e)_
---
+++ main.js
@@ -0,0 +1,50 @@
+
+
+// Sets the number of stars we wish to display
+const numStars = 100;
+
+// For every star we want to display
+for (let i = 0; i < numStars; i++) {
+    let star = document.createElement("div");  
+    star.className = "star";
+    var xy = getRandomPosition();
+    star.style.top = xy[0] + 'px';
+    star.style.left = xy[1] + 'px';
+    star.style.zIndex = -1;
+    document.body.append(star);
+}
+
+// Gets random x, y values based on the size of the container
+function getRandomPosition() {  
+    var y = window.innerWidth;
+    var x = window.innerHeight;
+    var randomX = Math.floor(Math.random()*x);
+    var randomY = Math.floor(Math.random()*y);
+    return [randomX, randomY];
+}
+
+
+document.getElementById("go").onclick = function () {
+    document.getElementById("crawl-details").style.display = "none";
+    document.getElementById("crawl").style.display = "inline";
+
+    var input = document.getElementById("audio-input");
+    var audio = new Audio(input.value || input.placeholder);
+    audio.oncanplaythrough = function() {
+
+        var elements = ["intro", "logo", "title", "subtitle", "crawl"];
+
+        elements.forEach(function(element) {
+            var target = document.getElementById(element);
+            var input = document.getElementById(element + "-input");
+            if (element === "logo") {
+                target.innerHTML = input.value || input.placeholder;
+            } else {
+                target.innerText = input.value || input.placeholder;
+            };
+        });
+
+        document.getElementById("crawl-container").style.display = "block";
+        audio.play();
+    };
+}
